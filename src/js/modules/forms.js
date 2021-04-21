4b40a8dbeb09bec7de6551f79fd8a749d6037aa4
Yuri <dog-pit10@yandex.ru>
2021-04-21 14:06:04 +0300

    added modified modules

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,14 +1,11 @@
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
     //получаем элементы
     const form = document.querySelectorAll('form'),
-        inputs = document.querySelectorAll('input'),
-        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+        inputs = document.querySelectorAll('input');
 //проверка ввода телефона на цифры
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        })
-    })
+    checkNumInputs('input[name="user_phone"]');
 //объект с сообщениями,которые будут показываться пользователю
     const message = {
         loading: 'Загрузка...',
@@ -43,6 +40,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 //собираем все данные из формы
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === 'end') {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 // отправляем запрос на сервер
             postData('assets/server.php', formData)
                 .then(res => {
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,25 +1,38 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelector(closeSelector);
+            close = document.querySelector(closeSelector),
+            widows = document.querySelectorAll('[data-modal]');
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
                 }
+
+                widows.forEach(item => {
+                    item.style.display = 'none';
+                })
+
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
             })
         })
-
+//when you click on the cross
         close.addEventListener('click', () => {
+            widows.forEach(item => {
+                item.style.display = 'none';
+            })
             modal.style.display = 'none';
             document.body.style.overflow = '';
         })
+//when you click on the dark background
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+                widows.forEach(item => {
+                    item.style.display = 'none';
+                })
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
             }
@@ -35,6 +48,10 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+
     // showModalByTime('.popup',60000);
 }
 
